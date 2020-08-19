7144542ea6b5e5dfa2baafad0196e2e734bb40ae
Rahul Sahay <rahulsahay19@hotmail.com>
2020-08-20 00:00:31 +0530

    initial commit

---
+++ MovieDbProject/dbo/Tables/Movies.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Movies] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX) NULL,
+    [DirectorName] NVARCHAR (MAX) NULL,
+    [ReleaseYear]  NVARCHAR (MAX) NULL,
+    [Genre]        NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Movies] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- MoviesApp/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:5001/api/'
-  //apiUrl: 'http://moviesapi.eastus.azurecontainer.io/api/'
-};
