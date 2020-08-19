877961c59fe0ab413fef3487566e2b3dcfa8ae72
Rahul Sahay <rahulsahay19@hotmail.com>
2020-08-19 23:57:08 +0530

    initial commit

--- SampleWebApp/Startup.cs
+++ SampleWebApp/Startup.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace MoviesAPI
+namespace SampleWebApp
 {
     public class Startup
     {
@@ -19,15 +26,6 @@ namespace MoviesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<MoviesService>();
-
-             services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", policy =>
-                {
-                   policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,11 +36,9 @@ namespace MoviesAPI
                 app.UseDeveloperExceptionPage();
             }
 
-          //  app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
